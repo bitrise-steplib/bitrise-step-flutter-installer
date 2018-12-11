c4f84db145299e445642df00edf7cd5443be90ee
Papik Tamas <trapacska@gmail.com>
2018-12-11 15:58:12 +0100

    PR changes

--- main.go
+++ main.go
@@ -3,21 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 
-	"github.com/bitrise-tools/go-steputils/tools"
-
 	"github.com/bitrise-io/go-utils/command"
-	"github.com/bitrise-io/go-utils/log"
-
 	"github.com/bitrise-io/go-utils/command/git"
+	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-tools/go-steputils/stepconf"
+	"github.com/bitrise-tools/go-steputils/tools"
 )
 
 type config struct {
-	Version   string `env:"version,required"`
-	Overwrite bool   `env:"overwrite,required"`
+	Version string `env:"version,required"`
 }
 
 func failf(msg string, args ...interface{}) {
@@ -32,45 +28,38 @@ func main() {
 	}
 	stepconf.Print(cfg)
 
-	isInstalled := true
-	if _, err := exec.LookPath("flutter"); err != nil {
-		isInstalled = false
+	fmt.Println()
+	log.Infof("Downloading Flutter SDK")
+	log.Printf("git clone")
+
+	sdkLocation := filepath.Join(os.Getenv("HOME"), "flutter-sdk")
+
+	if err := os.RemoveAll(sdkLocation); err != nil {
+		failf("Failed to remove path(%s), error: %s", sdkLocation, err)
 	}
 
-	if cfg.Overwrite || !isInstalled {
-		fmt.Println()
-		log.Infof("Downloading Flutter SDK")
-		log.Printf("git clone")
-
-		sdkLocation := filepath.Join(os.Getenv("HOME"), "flutter-sdk")
-
-		if err := os.RemoveAll(sdkLocation); err != nil {
-			failf("Failed to remove path(%s), error: %s", sdkLocation, err)
-		}
-
-		gitRepo, err := git.New(sdkLocation)
-		if err != nil {
-			failf("Failed to open git repo, error: %s", err)
-		}
-
-		if err := gitRepo.CloneTagOrBranch("https://github.com/flutter/flutter.git", cfg.Version).Run(); err != nil {
-			failf("Failed to clone git repo for tag/branch: %s, error: %s", cfg.Version, err)
-		}
-
-		log.Printf("set in $PATH")
-
-		path := filepath.Join(sdkLocation, "bin") + ":" + os.Getenv("PATH")
-
-		if err := os.Setenv("PATH", path); err != nil {
-			failf("Failed to set env, error: %s", err)
-		}
-
-		if err := tools.ExportEnvironmentWithEnvman("PATH", path); err != nil {
-			failf("Failed to export env with envman, error: %s", err)
-		}
-
-		log.Donef("Done")
+	gitRepo, err := git.New(sdkLocation)
+	if err != nil {
+		failf("Failed to open git repo, error: %s", err)
 	}
+
+	if err := gitRepo.CloneTagOrBranch("https://github.com/flutter/flutter.git", cfg.Version).Run(); err != nil {
+		failf("Failed to clone git repo for tag/branch: %s, error: %s", cfg.Version, err)
+	}
+
+	log.Printf("set in $PATH")
+
+	path := filepath.Join(sdkLocation, "bin") + ":" + os.Getenv("PATH")
+
+	if err := os.Setenv("PATH", path); err != nil {
+		failf("Failed to set env, error: %s", err)
+	}
+
+	if err := tools.ExportEnvironmentWithEnvman("PATH", path); err != nil {
+		failf("Failed to export env with envman, error: %s", err)
+	}
+
+	log.Donef("Done")
 
 	fmt.Println()
 	log.Infof("Flutter version")
